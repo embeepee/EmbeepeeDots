22e5e8a1bc2fc6050e50ad884e46f9d0a2b6dc5f
embeepee <markohblpabellano@gmail.com>
2025-11-18 01:39:28 +0800

    doom emacs initial configuration

---
+++ dot_config/doom/custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5" default))
+ '(org-agenda-files
+   '("~/mnt/98e3bc18-f06d-4d5c-b32d-c4b53446dd8e/myorg/roam/20251102180729-2025_week_44.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
